be35715e65214b2becab48c7b2b29159a0ed555c
redhayes <camihayes72@gmail.com>
2015-08-20 21:11:52 +0000

    corrected an error

--- app/models/review.rb
+++
@@ -1,5 +0,0 @@
-class Review < ActiveRecord::Base
-  belongs_to :chef
-  belongs_to :recipe
-  validates :body, presence: true, length: { minimum: 6, maximum: 500 }
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
  resources :recipes do
     member do
       post 'like'
-      post 'review'
-      delete 'review', to: "recipes#deletereview"
     end
   end
   
